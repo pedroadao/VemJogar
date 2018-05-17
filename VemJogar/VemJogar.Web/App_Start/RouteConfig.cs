935a89b8c15a44c6ae88b5dfd6978efcfd8a6503
Pedro Adão <pedro.adao.arruda@gmail.com>
2018-05-17 00:46:30 -0300

    Alteracoes

--- VemJogar/VemJogar.Web/App_Start/RouteConfig.cs
+++ VemJogar/VemJogar.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace VemJogar.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Authentication", action = "PreAuth", id = UrlParameter.Optional }
             );
         }
     }
